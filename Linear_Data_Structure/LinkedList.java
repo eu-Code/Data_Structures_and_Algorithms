ad9fd0e814e7afe6f70192fcb77e80a818ba1655
Efriem <efidesalew@gmail.com>
2021-02-20 11:49:55 +0300

    SLNode and LinkedList

---
+++ Linear_Data_Structure/LinkedList.java
@@ -0,0 +1,38 @@
+public class LinkedList(){
+	private SLNode head; // head node
+	private SLNode tail; // tail node
+	public LinkedList(){
+		this.head = null;
+		this.tail = null;:
+	}
+	// add to head
+	public void  addToHead(SLNode node){
+		// write your implementation here
+	}
+	// add to tail
+	public void addToTail(SLNode node){
+		// write your implementation here
+	}
+	// remove from head
+	public void removeFromHead(){
+		// write your implementation here
+	}
+	// remove from tail
+	public void removeFromTail(){
+		// write your implementation here
+	}
+	// remove from anywhere
+	public void remove(SLNode node){
+		// write your implementation here
+	}
+	// search node
+	public void search(SLNode node){
+		// write your implementation here
+	}
+	//print list
+	public void printList(){
+		// write your implementation here
+	}
+	
+	
+}
---
+++ Linear_Data_Structure/SLNode.java
@@ -0,0 +1,8 @@
+public class SLNode{
+	// define SL Attributes here
+	public SLNode(){
+	
+	}
+	//define getters here
+	//define setters here
+}
