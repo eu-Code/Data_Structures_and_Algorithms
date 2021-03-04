1b175992512fc373926d04aeb89b9b55158ad430
Efriem <efidesalew@gmail.com>
2021-03-04 21:31:04 +0300

    vist, search and inseret

--- Non_Linear_Data_Structures/BST.java
+++ Non_Linear_Data_Structures/BST.java
@@ -2,6 +2,32 @@ public class BST(){
 	private BSTNode root;
 	//
 	public BST(){
-	
+		this.root = null;	
+	}
+
+	public void visit(BSTNode p){
+		System.out.println(p.key+ " ");
+	}
+
+	public BSTNOde search(BSTNode p, int el){
+		while(p!=null){
+			if(el==p.key) return p;
+			else if(el<p.key) p = p.left;
+			else p=p.right;
+		}
+		return null;
+	}
+
+	public void insert(el){
+		BSTNode p = root;
+		BSTNode prev = null;
+		while(p!=null){
+			prev = p;
+			if(p.key<el) p = p.right;
+			else p = p.left;
+		}
+		if (this.root = = null) root = new BSTNode(el);
+		else if (prev.key<el) prev.right = new BSTNode(el);
+		else prev.left = new BSTNode(el);
 	}
 }
